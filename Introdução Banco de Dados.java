e9b17e95fcd10f4121063b05fc15a6f461a1582f
IgorProchmann <102532200+IgorProchmann@users.noreply.github.com>
2022-10-20 08:22:20 -0300

    Introdução a Banco de Dados

---
+++ Introdução Banco de Dados.java
@@ -0,0 +1,32 @@
+<h1>Trabalho feito no Google Docs<h1>
+
+
+<h2>1. Qual a diferença entre dados, informação e conhecimento?<h2>
+<h2>R: Dado - Fato do mundo real que está registrado.
+<h2>Informação - Fatos que possuem um significado em determinado contexto.<h2>
+<h2>Conhecimento - Quando a informação possui uma aplicação prática.<h2>
+<h2>Alcance de um resultado concreto a partir de determinado conteúdo.<h2>
+
+
+<h2>2. O que é um SGBD?<h2>
+R: É o sistema de software responsável pelo gerenciamento de um ou mais bancos de dados.<h2>
+
+<h2>3. O que é um banco de dados relacional?<h2>
+R: É um banco de dados que modela os dados de uma forma que eles sejam percebidos pelo usuário<h2>
+
+<h2>4. O que é um banco de dados não relacional?<h2>
+R: É um banco de dados que não usa o esquema de tabela de linhas e colunas<h2>
+
+<h2>5. Quais são os benefícios de um banco de dados relacional?<h2>
+R: São altamente estruturados e têm suportes teóricos consolidados da computação<h2>
+
+<h2>6. Quais são os benefícios de um banco de dados não relacional?<h2>
+<h2>R: Oferecem mais escalabilidade, melhor performance em queries e flexibilidade para manipular dados que podem apresentar mudanças em seu formato.<h2>
+
+<h2>7. Evidencie as diferenças entre os conceitos de BD e SGBD<h2>
+<h2>R: SGBD deverá apresentar facilidade para recuperar falhas de hardware e software, através da existência de arquivos de “pré-imagem” ou de outros recursos automáticos, exigindo minimamente a intervenção de pessoal técnico.<h2>
+<h2>O Sistema de Banco de dados deve prover uma visão abstrata dos dados para os usuários.<h2>
+
+
+<h2>8. Problema: preciso organizar meus gastos mensais. Defina as informações importantes da tabela.<h2>
+<h2>R: Limite de dinheiro(Salario), Dias do mês, quantidade dos gastos diários,<h2>
